d56419cb49dff928b3d84a35746f639d2364533e
Thomas <denhollander.thomas@gmail.com>
2022-12-10 21:46:07 +0100

    Day 8

--- src/day8.rs
+++ src/day8.rs
@@ -1,11 +1,7 @@
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Default)]
 struct Index<const WIDTH: usize>(usize);
 
 impl<const WIDTH: usize> Index<WIDTH> {
-    fn from_coords(x: usize, y: usize) -> Self {
-        Index(x + y * (WIDTH + 1))
-    }
-
     fn move_left(&mut self) {
         self.0 -= 1;
     }
@@ -27,8 +23,8 @@ pub fn part_1_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
     s: &str,
 ) -> usize {
     let trees = s.as_bytes();
-    assert!(trees.len() >= WIDTH * HEIGHT);
-    debug_assert!(TOTAL_SIZE == (WIDTH + 1) * HEIGHT);
+    assert!(trees.len() >= TOTAL_SIZE);
+    debug_assert!(TOTAL_SIZE == (WIDTH + 1) * (HEIGHT - 1) + WIDTH);
     let mut marked = [false; TOTAL_SIZE];
     let mut test_and_mark = |index: Index<WIDTH>, current_smallest: &mut u8| {
         let tree = trees[index.0];
@@ -38,9 +34,9 @@ pub fn part_1_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
         }
     };
 
-    for x in 0..WIDTH {
+    let mut index = Index::default();
+    for _ in 0..WIDTH {
         let mut current_smallest = b'0' - 1;
-        let mut index = Index::from_coords(x, 0);
         for _ in 0..HEIGHT {
             test_and_mark(index, &mut current_smallest);
             index.move_down();
@@ -51,11 +47,12 @@ pub fn part_1_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
             index.move_up();
             test_and_mark(index, &mut current_smallest);
         }
+        index.move_right();
     }
 
-    for y in 0..HEIGHT {
+    index = Index::default();
+    for _ in 0..HEIGHT {
         let mut current_smallest = b'0' - 1;
-        let mut index = Index::from_coords(0, y);
         for _ in 0..WIDTH {
             test_and_mark(index, &mut current_smallest);
             index.move_right();
@@ -66,6 +63,7 @@ pub fn part_1_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
             index.move_left();
             test_and_mark(index, &mut current_smallest);
         }
+        index.move_down();
     }
     marked.iter().filter(|b| **b).count()
 }
@@ -74,6 +72,8 @@ pub fn part_2_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
     s: &str,
 ) -> usize {
     let trees = s.as_bytes();
+    assert!(trees.len() >= TOTAL_SIZE);
+    debug_assert!(TOTAL_SIZE == (WIDTH + 1) * (HEIGHT - 1) + WIDTH);
 
     // Set indexed like the trees.
     let mut scenic_score = [1usize; TOTAL_SIZE];
@@ -81,7 +81,7 @@ pub fn part_2_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
     let mut test_and_mark =
         |index: Index<WIDTH>,
          position_along_line: usize,
-         last_encounter_of_tree_of_at_least_height: &mut [usize; 10]| {
+         last_encounter_of_tree_of_at_least_height: &mut [usize; 16]| {
             let tree = usize::from(trees[index.0] & 0b1111);
             // Compute the view
             let view = position_along_line - last_encounter_of_tree_of_at_least_height[tree];
@@ -94,42 +94,45 @@ pub fn part_2_generic<const WIDTH: usize, const HEIGHT: usize, const TOTAL_SIZE:
             }
         };
 
-    for x in 0..WIDTH {
-        let mut index = Index::from_coords(x, 0);
-        let mut last_encounter_of_tree_of_at_least_height = [0; 10];
+    let mut last_encounter_of_tree_of_at_least_height;
+    let mut index = Index::<WIDTH>::default();
+    for _ in 0..WIDTH {
+        last_encounter_of_tree_of_at_least_height = [0; 16];
         for y in 0..HEIGHT {
             test_and_mark(index, y, &mut last_encounter_of_tree_of_at_least_height);
             index.move_down();
         }
-        let mut last_encounter_of_tree_of_at_least_height = [0; 10];
+        last_encounter_of_tree_of_at_least_height = [0; 16];
         for y in 0..HEIGHT {
             index.move_up();
             test_and_mark(index, y, &mut last_encounter_of_tree_of_at_least_height);
         }
+        index.move_right();
     }
 
-    for y in 0..HEIGHT {
-        let mut index = Index::from_coords(0, y);
-        let mut last_encounter_of_tree_of_at_least_height = [0; 10];
+    index = Index::<WIDTH>::default();
+    for _ in 0..HEIGHT {
+        last_encounter_of_tree_of_at_least_height = [0; 16];
         for x in 0..WIDTH {
             test_and_mark(index, x, &mut last_encounter_of_tree_of_at_least_height);
             index.move_right();
         }
-        let mut last_encounter_of_tree_of_at_least_height = [0; 10];
+        last_encounter_of_tree_of_at_least_height = [0; 16];
         for x in 0..WIDTH {
             index.move_left();
             test_and_mark(index, x, &mut last_encounter_of_tree_of_at_least_height);
         }
+        index.move_down();
     }
     scenic_score.iter().copied().max().unwrap_or_default()
 }
 
 pub fn part_1(s: &str) -> usize {
-    part_1_generic::<99, 99, 9_900>(s)
+    part_1_generic::<99, 99, 9899>(s)
 }
 
 pub fn part_2(s: &str) -> usize {
-    part_2_generic::<99, 99, 9_900>(s)
+    part_2_generic::<99, 99, 9899>(s)
 }
 
 #[test]
@@ -139,7 +142,7 @@ fn test_part_1_example() {
 65332
 33549
 35390";
-    assert_eq!(part_1_generic::<5, 5, 30>(input), 21);
+    assert_eq!(part_1_generic::<5, 5, 29>(input), 21);
 }
 
 #[test]
@@ -149,7 +152,7 @@ fn test_part_2_example() {
 65332
 33549
 35390";
-    assert_eq!(part_2_generic::<5, 5, 30>(input), 8);
+    assert_eq!(part_2_generic::<5, 5, 29>(input), 8);
 }
 
 #[test]
